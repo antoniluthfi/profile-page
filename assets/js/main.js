655908bb224d1c06bf13565397065245b9896cf0
antoniluthfi <luthfiantoni027@gmail.com>
2021-06-23 13:47:08 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,118 @@
+// show menu
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+    if(toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show');
+        });
+    }
+}
+
+showMenu('nav-toggle', 'nav-menu');
+
+// select menu
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    // active link
+    navLink.forEach((n) => n.classList.remove('active'));
+    this.classList.add('active');
+
+    // remove menu
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show');
+}
+
+navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+// skills data
+const skills = [
+    {
+        "icon": "html5",
+        "name": "HTML5",
+        "percentage": "95%"
+    },
+    {
+        "icon": "css3",
+        "name": "CSS3",
+        "percentage": "80%"
+    },
+    {
+        "icon": "javascript",
+        "name": "JavaScript",
+        "percentage": "70%"
+    },
+    {
+        "icon": "nodejs",
+        "name": "Node JS",
+        "percentage": "70%"
+    },
+    {
+        "icon": "react",
+        "name": "React JS",
+        "percentage": "65%"
+    },
+];
+
+let skillsHTML = '';
+skills.forEach((skill) => {
+    skillsHTML += `
+        <div class="skills__data">
+            <div class="skills__names">
+                <i class="bx bxl-${skill.icon} skills__icon"></i>
+                <span class="skills__name">${skill.name}</span>
+            </div>
+            <div>
+                <span class="skills_percentage">${skill.percentage}</span>
+            </div>
+            <div class="skills__bar" style="width: ${skill.percentage};"></div>
+        </div>
+    `;
+});
+
+document.getElementById('skills-list').innerHTML = skillsHTML;
+
+// work
+let workHTML = '';
+for (let i = 1; i <= 6; i++) {
+    workHTML += `
+        <div class="work__img">
+            <img src="assets/images/work${i}.jpg" alt="">
+        </div>
+    `;
+}
+
+document.getElementById('work-content').innerHTML = workHTML;
+
+// scroll reveal animation
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 1000,
+    reset: true
+});
+
+// scroll home
+sr.reveal('.home__title',{}); 
+sr.reveal('.button',{delay: 200}); 
+sr.reveal('.home__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+
+// scroll about
+sr.reveal('.about__img',{}); 
+sr.reveal('.about__subtitle',{delay: 400}); 
+sr.reveal('.about__text',{delay: 400}); 
+
+// scroll skills
+sr.reveal('.skills__subtitle',{}); 
+sr.reveal('.skills__text',{}); 
+sr.reveal('.skills__data',{interval: 200}); 
+sr.reveal('.skills__img',{delay: 600});
+
+// scroll skills
+sr.reveal('.work__img',{interval: 200}); 
+
+// scroll contact
+sr.reveal('.contact__input',{interval: 200}); 
